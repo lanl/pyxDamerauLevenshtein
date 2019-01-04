eb4d6118f36cefde1a514e38f8367d64644ed519
Geoffrey Fairchild <gfairchild@lanl.gov>
2019-01-04 15:51:19 -0700

    attempt 2

--- setup.py
+++ setup.py
@@ -77,6 +77,7 @@ metadata = dict(
 )
 
 setup(
+    setup_requires=['numpy'],
     install_requires=['numpy'],
     ext_modules=[Extension('pyxdameraulevenshtein', ['pyxdameraulevenshtein/pyxdameraulevenshtein.c'])],
     **metadata
