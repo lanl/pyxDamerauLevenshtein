56cfda25b62bd128da3829f3a436b77e1a3f3c00
Geoffrey Fairchild <gfairchild@lanl.gov>
2019-03-08 12:05:25 -0700

    LANS -> Triad and copyright assertion

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 """
-    Copyright (c) 2013, Los Alamos National Security, LLC
+    Copyright (c) 2013, Triad National Security, LLC
     All rights reserved.
 
     Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
@@ -9,7 +9,7 @@
       disclaimer.
     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
       following disclaimer in the documentation and/or other materials provided with the distribution.
-    * Neither the name of Los Alamos National Security, LLC nor the names of its contributors may be used to endorse or
+    * Neither the name of Triad National Security, LLC nor the names of its contributors may be used to endorse or
       promote products derived from this software without specific prior written permission.
 
     THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
--- tests/test_pyxdl.py
+++ tests/test_pyxdl.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-    Copyright (c) 2016, Los Alamos National Security, LLC
+    Copyright (c) 2016, Triad National Security, LLC
     All rights reserved.
 
     Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
@@ -12,7 +12,7 @@
       disclaimer.
     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
       following disclaimer in the documentation and/or other materials provided with the distribution.
-    * Neither the name of Los Alamos National Security, LLC nor the names of its contributors may be used to endorse or
+    * Neither the name of Triad National Security, LLC nor the names of its contributors may be used to endorse or
       promote products derived from this software without specific prior written permission.
 
     THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
